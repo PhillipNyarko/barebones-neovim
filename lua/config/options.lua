fd9072cbfa409384fe9e795b820fcce160b673a5
Phillip Nyarko <Phillip.k.Nyarko@gmail.com>
2024-02-09 20:51:20 -0600

    options updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,4 @@ vim.opt.mouse = "" -- disable right-click mouse menu
 vim.opt.scrolloff = 8 -- set vertical scroll offset to 8 lines
 vim.opt.undofile = true -- allow for undo even after exiting vim
 vim.opt.termguicolors = true -- allows for more default color schemes
+vim.opt.cursorline = true -- Enable cursor line highlighting
